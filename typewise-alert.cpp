582548f075149438ebb7f2efaceafd17e18c1e70
Nagaraj Anusha (MS/ESD-NR8-XC) <Anusha.Jagadeesha@in.bosch.com>
2022-02-22 21:53:14 +0530

    added UT

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -2,18 +2,22 @@
 #include "map_alertTarget.h"
 #include <stdio.h>
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
+BreachType inferBreach(double value, double lowerLimit, double upperLimit)
+{
+  if (value < lowerLimit)
+  {
     return TOO_LOW;
   }
-  if(value > upperLimit) {
+  if (value > upperLimit)
+  {
     return TOO_HIGH;
   }
   return NORMAL;
 }
 
 BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
+    CoolingType coolingType, double temperatureInC)
+{
   int lowerLimit = 0;
   int upperLimit = 0;
   lowerLimit = classifyTempBreachMap.find(coolingType)->second.first;
@@ -22,25 +26,33 @@ BreachType classifyTemperatureBreach(
 }
 
 bool checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC)
+{
 
   BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
+      batteryChar.coolingType, temperatureInC);
 
-pfun f = alertTargetMsg.find(alertTarget)->second;
-return ((*f)(breachType)); 
+  pfun f = alertTargetMsg.find(alertTarget)->second;
+  return ((*f)(breachType));
 }
 
-bool sendToController(BreachType breachType) {
+bool sendToController(BreachType breachType)
+{
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
   return true;
 }
 
-// bool sendToEmail(BreachType breachType) {
-//   const char* recepient = "a.b@c.com";
-//   printf("To: %s\n", recepient);
-//   cout << sendToEmailMap.find(breachType)->second << "\n";
-//   return true;
-// }
+bool sendToEmail(BreachType breachType)
+{
+  const char *recepient = "a.b@c.com";
+  if (sendToEmailMap.find(breachType)->first < (static_cast<BreachType>(3)))
+  {
+    cout << "To: " << recepient << "    " << sendToEmailMap.find(breachType)->second << "\n";
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
